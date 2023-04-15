8b07db233e969885ef0cbbf55ca2f2d79e586e43
abhipunj01 <abhishek.punj@dentsu.com>
2023-04-15 23:31:08 +0530

    End to end automation

---
+++ src/test/java/abhiLearns/pojo/createOrder/CreateOrderRequest.java
@@ -0,0 +1,15 @@
+package abhiLearns.pojo.createOrder;
+
+import java.util.List;
+
+public class CreateOrderRequest {
+    private List<Orders> orders;
+
+    public List<Orders> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Orders> orders) {
+        this.orders = orders;
+    }
+}
